635473835b334445ac142021d5cc81195d5fcecc
Salvador Barrionuevo Gutierrez <salvador.barrionuevo.g@uni.pe>
2025-10-03 14:09:40 +0000

    estructura del repositorio mejorada

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -6,7 +6,6 @@ DATA_DIR = BASE_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
 __all__ = [
     "BASE_DIR",
